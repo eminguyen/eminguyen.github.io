60c5fb880596be4e1043d5c5ce2d2dec3580bbec
eminguyen <emilyhuongnguyen@gmail.com>
2018-05-20 23:51:00 -0700

    Customize typing animation/text

--- index.js
+++ index.js
@@ -77,10 +77,10 @@ $(document).ready(function() {
 
 $('document').ready(function(){
   var typed = new Typed("#typed", {
-    strings: ["This is a JavaScript library", "This is an ES6 module"],
-    typeSpeed: 0,
-    backSpeed: 0,
-    smartBackspace: true, // this is a default
+    strings: ['developer','designer','runner','gamer','transgender'],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 3000,
     loop: true
   });
 })
