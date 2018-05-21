7eae13a75496d4cc4d23b3619ee649e72bacda17
eminguyen <emilyhuongnguyen@gmail.com>
2018-05-21 11:37:16 -0700

    Add wow.js / animate.js test

--- index.js
+++ index.js
@@ -74,16 +74,15 @@ $(document).ready(function() {
 		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
 		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
 	});
-});
 
-/*Typing animation for intro screen*/
-
-$('document').ready(function(){
-  var typed = new Typed("#typed", {
+	/*Typing animation for intro screen*/
+	var typed = new Typed("#typed", {
     strings: ['developer','designer','runner','gamer','transgender'],
     typeSpeed: 75,
     backSpeed: 75,
     backDelay: 3000,
     loop: true
   });
-})
+
+	new WOW().init();
+});
