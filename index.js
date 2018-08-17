2e97cc352e64dccdcf06e51b1997a8c100022691
eminguyen <emilyhuongnguyen@gmail.com>
2018-08-17 01:41:35 -0700

    Remove fullPage.js and WoW.js; redo intro page and added bubble background

--- index.js
+++ index.js
@@ -1,79 +1,6 @@
 /*fullPage.js initialization for scrolling*/
 
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		//Navigation
-		menu: '#menu',
-		lockAnchors: false,
-		anchors:['firstPage', 'secondPage'],
-		navigation: false,
-		navigationPosition: 'right',
-		navigationTooltips: ['firstSlide', 'secondSlide'],
-		showActiveTooltip: false,
-		slidesNavigation: true,
-		slidesNavPosition: 'bottom',
-
-		//Scrolling
-		css3: true,
-		scrollingSpeed: 700,
-		autoScrolling: true,
-		fitToSection: true,
-		fitToSectionDelay: 1000,
-		scrollBar: true,
-		easing: 'easeInOutCubic',
-		easingcss3: 'ease',
-		loopBottom: true,
-		loopTop: true,
-		loopHorizontal: true,
-		continuousVertical: true,
-		continuousHorizontal: false,
-		scrollHorizontally: false,
-		interlockedSlides: false,
-		dragAndMove: false,
-		offsetSections: false,
-		resetSliders: false,
-		fadingEffect: false,
-		normalScrollElements: '#element1, .element2',
-		scrollOverflow: false,
-		scrollOverflowReset: false,
-		scrollOverflowOptions: null,
-		touchSensitivity: 15,
-		normalScrollElementTouchThreshold: 5,
-		bigSectionsDestination: null,
-
-		//Accessibility
-		keyboardScrolling: true,
-		animateAnchor: true,
-		recordHistory: true,
-
-		//Design
-		controlArrows: true,
-		verticalCentered: false,
-		sectionsColor : '#fff',
-		paddingTop: '0em',
-		paddingBottom: '0em',
-		fixedElements: '#header, .footer',
-		responsiveWidth: 0,
-		responsiveHeight: 0,
-		responsiveSlides: false,
-		parallax: true,
-		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-
-		//Custom selectors
-		sectionSelector: '.section',
-		slideSelector: '.slide',
-
-		lazyLoading: true,
-
-		//events
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-		afterResize: function(){},
-		afterResponsive: function(isResponsive){},
-		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-	});
 
 	/*Typing animation for intro screen*/
 	var typed = new Typed("#typed", {
@@ -84,5 +11,4 @@ $(document).ready(function() {
     loop: true
   });
 
-	new WOW().init();
 });
