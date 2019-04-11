f69602496755013d4acbc55bba5f70495aeb7ae9
eminguyen <emilyhuongnguyen@gmail.com>
2019-04-11 11:59:00 -0700

    Make div follow screen and add strikethrough through bucket list entries

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -1,12 +1,20 @@
-let fixmeTop = $('.info').offset().top - ($(window).height()/2 - $(window).width()/5);
+let fixmeTop = $('.info').offset().top - ($(window).height() * .5 - $(window).width() * .25);
+let bottom = $('#bucket-list').offset().top + $('#bucket-list').outerHeight(true) - ($(window).height() * .5 - $(window).width() * .25);
+console.log(bottom)
 
 $(window).scroll(function() {
     var currentScroll = $(window).scrollTop();
 
     if (currentScroll >= fixmeTop) {
+      console.log(currentScroll + $(window).height())
+      if(currentScroll + $(window).height() >= bottom) {
+        $('.info').attr('id', 'bottom');
+        console.log('ok')
+      }
+      else {
         $('.info').attr('id', 'fix-it');
+      }
     } else {
         $('.info').attr('id', '');
     }
-
 });
