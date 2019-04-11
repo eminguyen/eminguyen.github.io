3ef5fd026245a3c5ac0f5e8fcded80dde7058e9f
eminguyen <emilyhuongnguyen@gmail.com>
2019-04-11 13:08:19 -0700

    Add changing pictures

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -18,20 +18,35 @@ $(window).scroll(function() {
 });
 
 let selected;
+const complete = $('.complete');
+randomlyChange();
+let randomizer = setInterval(randomlyChange, 5000);
 
 $('.complete').click(function(e) {
+  clearInterval(randomizer);
   selected = $(this);
   changeContent();
 });
 
+function randomlyChange() {
+  selected = $(complete[Math.floor(Math.random()*complete.length)])
+  changeContent();
+}
+
 function changeContent() {
   let title = selected.html();
   let id = selected.attr('id');
+  let image = selected.data().content;
+  console.log(selected.data());
   $('#info-header').fadeOut(200, function() {
-    $('#info-header').html(title).fadeIn(200);;
+    $('#info-header').html(title).fadeIn(200);
   });
 
   $('#info-content').fadeOut(200, function() {
-    $('#info-content').html($(`.${id}`).html()).fadeIn(200);;
+    $('#info-content').html($(`.${id}`).html()).fadeIn(200);
+  });
+
+  $('#info-image').fadeOut(200, function() {
+    $('#info-image').attr("src",image).fadeIn(200);
   });
 }
